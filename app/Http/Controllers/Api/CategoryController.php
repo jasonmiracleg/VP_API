24779aa7594631cd5603088ff751c8f31af0708d
jasonmiracleg <jasonmiracle15@gmail.com>
2024-01-03 20:20:06 +0700

    Add Category Resource

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
-    //
+    public function createCategory(Request $request)
+    {
+        $category = new Category();
+        $category = $request->title;
+        $category = $request->color;
+        $category->save();
+    }
 }
