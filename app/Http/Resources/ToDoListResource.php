e5b0479e86dfa877f7a32838243c86258d7f6636
jasonmiracleg <jasonmiracle15@gmail.com>
2024-01-10 09:55:54 +0700

    To Do List Update

--- app/Http/Resources/ToDoListResource.php
+++ app/Http/Resources/ToDoListResource.php
@@ -15,16 +15,15 @@ class ToDoListResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'is_group' => $this->is_group,
             'is_complete' => $this->is_complete,
             'description' => $this->description,
             'timer' => $this->timer,
             'total_seconds' => $this->total_seconds,
-            'elapsed' => $this->elapsed,
             'timer_started' => $this->timer_started,
             'user_id' => $this->user_id,
-            'reminder_id' => $this->reminder_id,
             'grouping_id' => $this->grouping_id,
             'date' => $this->date,
             'day' => $this->day
